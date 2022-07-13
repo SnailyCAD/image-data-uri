9c7672c10a90922dde9b28b81aa586f05d24c1f6
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-13 09:28:04 +0200

    fix(types): set options optional

--- lib/image-data-uri.d.ts
+++ lib/image-data-uri.d.ts
@@ -11,5 +11,5 @@ interface Options {
 export function decode(dataURI: string): ImageData;
 export function encode(data: ImageData, mediaType: string): string;
 export function encodeFromURL(filePath: string): Promise<string>;
-export function encodeFromFile(imageURL: string, options: Options): Promise<string>;
+export function encodeFromFile(imageURL: string, options?: Partial<Options>): Promise<string>;
 export function outputFile(dataURI: string, filePath: string): Promise<string>;
